c4556a8ef79bd86c9ee1cfcaea83f59ff0e8e385
r.kawano <r.kawano@genbasupport.com>
2024-10-29 15:26:51 +0900

    登録情報に郵便番号と住所、自己紹介文を追加 + アカウント編集ページを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name post_code address profile])
+  end
 end
