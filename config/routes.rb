4593bf35eeb11c62d9f0f4319d47b7685145f3c5
r.kawano <r.kawano@genbasupport.com>
2024-10-29 10:09:26 +0900

    パスワードの再設定機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   }
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   # Defines the root path route ("/")
   root 'books#index'
