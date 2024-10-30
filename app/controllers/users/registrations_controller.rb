a258bea0cc3b1d90017302627b628153dc8c8e07
r.kawano <r.kawano@genbasupport.com>
2024-10-30 14:30:10 +0900

    devise関連のメソッドを修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     books_path
   end
 end
