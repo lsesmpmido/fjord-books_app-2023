44ccac181f22241328eea9a28a463b7b69426808
r.kawano <r.kawano@genbasupport.com>
2024-11-07 14:46:23 +0900

    本およびレポートに対するコメント機能の追加

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  def create
+    @comment = @commentable.comments.build(comment_params)
+    @comment.user = current_user
+    return unless @comment.save
+
+    redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
