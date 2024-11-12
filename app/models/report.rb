cc14e75f19a3c638c75ef55f145291e68c4cea6f
r.kawano <r.kawano@genbasupport.com>
2024-11-12 14:02:46 +0900

    言及機能用の中間テーブルを作成

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,10 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :report_mentions, foreign_key: :mentioning_report_id, dependent: :destroy
+  has_many :mentioned_reports, through: :report_mentions, source: :mentioned_report
+  has_many :report_mentions, foreign_key: :mentioned_report_id, dependent: :destroy
+  has_many :mentioning_reports, through: :report_mentions, source: :mentioning_report
 
   validates :title, presence: true
   validates :content, presence: true
