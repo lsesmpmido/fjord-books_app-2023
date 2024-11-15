906d033dcbf35fae3761f8a8f42a86c12fb105d6
r.kawano <r.kawano@genbasupport.com>
2024-11-15 14:12:25 +0900

    インスタンスメソッド用のテストコードを作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -2,8 +2,37 @@
 
 require 'test_helper'
 
-class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class ReportTest < Test::Unit::TestCase
+  def setup
+    @me = User.find_or_create_by(email: 'me@example.com') do |user|
+      user.password = 'password'
+    end
+    @you = User.find_or_create_by(email: 'you@example.com') do |user|
+      user.password = 'password'
+    end
+    @report = @me.reports.create(title: 'title', content: 'content')
+    @mentioned_report = @me.reports.create(title: 'title', content: 'http://localhost:3000/reports/3')
+  end
+
+  sub_test_case '編集可能か判定' do
+    test '日報の作成者ならtrueを返す' do
+      assert_equal true, @report.editable?(@me)
+    end
+
+    test '日報の作成者でないならfalseを返す' do
+      assert_equal false, @report.editable?(@you)
+    end
+  end
+
+  sub_test_case '作成した日付を返す' do
+    test '日報を今日作成したなら今日の日付を返す' do
+      assert_equal Time.zone.today, @report.created_on
+    end
+  end
+
+  sub_test_case 'メンションを保存' do
+    test '日報に1つメンションを付けて作成すると1を返す' do
+      assert_equal 1, @mentioned_report.mentioning_reports.ids.size
+    end
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,8 +2,20 @@
 
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class UserTest < Test::Unit::TestCase
+  def setup
+    @user = User.new(email: 'hoge@example.com')
+  end
+
+  sub_test_case 'nameまたはemailを返す' do
+    test 'nameがnilならemailを返す' do
+      @user.name = nil
+      assert_equal 'hoge@example.com', @user.name_or_email
+    end
+
+    test 'nameが存在するならnameを返す' do
+      @user.name = 'hoge'
+      assert_equal 'hoge', @user.name_or_email
+    end
+  end
 end
