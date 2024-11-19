c09dfd9a0e82461b02403b74145b47d4dbb4ae9b
r.kawano <r.kawano@genbasupport.com>
2024-11-19 16:25:11 +0900

    トランザクション内でエラーが生じた場合にロールバックするように修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -20,23 +20,24 @@ class ReportsController < ApplicationController
   def edit; end
 
   def create
-    @report = current_user.reports.new(report_params)
-
-    if @report.save
+    ActiveRecord::Base.transaction do
+      @report = current_user.reports.new(report_params)
+      @report.save
       @report.create_mentions_from_content
       redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
-    else
-      render :new, status: :unprocessable_entity
     end
+  rescue ActiveRecord::Rollback
+    render :new, status: :unprocessable_entity
   end
 
   def update
-    if @report.update(report_params)
+    ActiveRecord::Base.transaction do
+      @report.update(report_params)
       @report.create_mentions_from_content
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
-    else
-      render :edit, status: :unprocessable_entity
     end
+  rescue ActiveRecord::Rollback
+    render :edit, status: :unprocessable_entity
   end
 
   def destroy
