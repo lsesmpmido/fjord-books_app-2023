a94179b5b17ae336719e8d7fe4ab02266de67d33
r.kawano <r.kawano@genbasupport.com>
2024-11-20 14:53:10 +0900

    修正漏れのモデルのテストコードを修正

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,20 +2,18 @@
 
 require 'test_helper'
 
-class UserTest < Test::Unit::TestCase
+class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(email: 'hoge@example.com')
   end
 
-  sub_test_case 'nameまたはemailを返す' do
-    test 'nameがnilならemailを返す' do
-      @user.name = nil
-      assert_equal 'hoge@example.com', @user.name_or_email
-    end
+  test 'nameがnilならemailを返す' do
+    @user.name = nil
+    assert_equal 'hoge@example.com', @user.name_or_email
+  end
 
-    test 'nameが存在するならnameを返す' do
-      @user.name = 'hoge'
-      assert_equal 'hoge', @user.name_or_email
-    end
+  test 'nameが存在するならnameを返す' do
+    @user.name = 'hoge'
+    assert_equal 'hoge', @user.name_or_email
   end
 end
