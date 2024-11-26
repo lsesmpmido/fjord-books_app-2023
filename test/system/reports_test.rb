01bd2a0cab04f23fd7c5ff046b5510798db0da9c
r.kawano <r.kawano@genbasupport.com>
2024-11-26 10:17:12 +0900

    fill_inやassert_textでfixturesの内容を使用しないように修正

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -4,8 +4,7 @@ require 'application_system_test_case'
 
 class ReportsTest < ApplicationSystemTestCase
   setup do
-    @create_report = reports(:create_report)
-    @update_report = reports(:update_report)
+    @sample_report = reports(:sample_report)
     visit root_url
     fill_in 'Eメール', with: 'alice@example.com'
     fill_in 'パスワード', with: 'password'
@@ -22,34 +21,34 @@ class ReportsTest < ApplicationSystemTestCase
     visit reports_url
     click_link '日報の新規作成'
 
-    fill_in 'タイトル', with: @create_report.title
-    fill_in '内容', with: @create_report.content
+    fill_in 'タイトル', with: '新規作成する日報のタイトル'
+    fill_in '内容', with: '新規作成する日報の内容'
     click_on '登録する'
 
     assert_selector 'h1', text: '日報の詳細'
-    assert_text @create_report.title
-    assert_text @create_report.content
+    assert_text '新規作成する日報のタイトル'
+    assert_text '新規作成する日報の内容'
     assert_text '日報が作成されました。'
     click_on '日報の一覧に戻る'
   end
 
   test 'レポートを更新' do
-    visit report_url(@create_report)
+    visit report_url(@sample_report)
     click_link 'この日報を編集'
 
-    fill_in 'タイトル', with: @update_report.title
-    fill_in '内容', with: @update_report.content
+    fill_in 'タイトル', with: '更新する日報のタイトル'
+    fill_in '内容', with: '更新する日報の内容'
     click_on '更新する'
 
     assert_selector 'h1', text: '日報の詳細'
-    assert_text @update_report.title
-    assert_text @update_report.content
+    assert_text '更新する日報のタイトル'
+    assert_text '更新する日報の内容'
     assert_text '日報が更新されました。'
     click_on '日報の一覧に戻る'
   end
 
   test 'レポートを削除' do
-    visit report_url(@update_report)
+    visit report_url(@sample_report)
     click_on 'この日報を削除'
 
     assert_selector 'h1', text: '日報の一覧'
